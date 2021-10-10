ea41fe0bbf5e18602a109e7713adf2d7f58dc1c9
elmtree-Ahn <clfclf3138@gmail.com>
2021-10-10 14:32:25 +0900

    JS 이벤트 위임 마무리

--- 이벤트위임공부/main.js
+++ 이벤트위임공부/main.js
@@ -1,4 +1,18 @@
-const btns = document.querySelectorAll('.menu-btn');
+const menu = document.querySelector('.menu');
 
 
-console.log(btns);
+function clickHandler(event){
+  let elem = event.target;
+  while (!elem.classList.contains('menu-btn')) {
+    elem = elem.parentNode;
+  
+    if (elem.nodeName = 'BODY') {
+      elem = null;
+      return;
+    }
+  }
+
+  console.log(elem.dataset.value);
+}
+
+menu.addEventListener('click', clickHandler)
